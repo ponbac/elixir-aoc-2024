1d617395418a96d3d17b463ba3dd1fe76c557bc1
ponbac <root@ponbac.xyz>
2024-12-21 10:15:25 +0100

    more functional

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -18,27 +18,39 @@ defmodule AdventOfCode.Day02 do
   end
 
   def is_valid?(list) do
-    pairs = Enum.chunk_every(list, 2, 1, :discard)
-
-    with true <- valid_differences?(pairs),
-         true <- consistent_direction?(pairs) do
+    with true <- valid_differences?(list),
+         true <- consistent_direction?(list) do
       true
     else
       _ -> false
     end
   end
 
-  def valid_differences?(pairs) do
-    Enum.all?(pairs, fn [a, b] ->
-      diff = abs(a - b)
-      diff >= 1 and diff <= 3
-    end)
+  def valid_differences?([a, b | rest]) do
+    diff = abs(a - b)
+    diff >= 1 and diff <= 3 and valid_differences?([b | rest])
   end
 
-  def consistent_direction?(pairs) do
-    directions = Enum.map(pairs, fn [a, b] -> b > a end)
-    Enum.all?(directions) or not Enum.any?(directions)
+  def valid_differences?(_) do
+    true
   end
+
+  def consistent_direction?([a, b | rest]) do
+    increasing = &(&1 < &2)
+    decreasing = &(&1 > &2)
+
+    cond do
+      increasing.(a, b) -> consistent_direction?([b | rest], increasing)
+      decreasing.(a, b) -> consistent_direction?([b | rest], decreasing)
+      true -> false
+    end
+  end
+
+  def consistent_direction?([a, b | rest], comparison) do
+    comparison.(a, b) and consistent_direction?([b | rest], comparison)
+  end
+
+  def consistent_direction?([_], _), do: true
 
   def parse_input(input) do
     input
